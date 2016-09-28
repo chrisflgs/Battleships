00426b6aee8bc8b3bf69ddecafe0d4b7ffd8f275
braydend <braydendiluca@gmail.com>
2016-09-28 14:06:00 +1000

    Fixed shot counter bug

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -181,17 +181,21 @@ public class Player : IEnumerable<Ship>
 	/// <returns>the result of the attack</returns>
 	internal AttackResult Shoot(int row, int col)
 	{
-		_shots += 1;
+		//_shots += 1;
 		AttackResult result = default(AttackResult);
 		result = EnemyGrid.HitTile(row, col);
 
 		switch (result.Value) {
 			case ResultOfAttack.Destroyed:
+				_shots += 1;
+				break;
 			case ResultOfAttack.Hit:
 				_hits += 1;
+				_shots += 1;
 				break;
 			case ResultOfAttack.Miss:
 				_misses += 1;
+				_shots += 1;
 				break;
 		}
 
